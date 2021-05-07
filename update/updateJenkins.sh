1ce950847a70da47e93d3abc40788bd7e96993c4
Alexander Link <alexander.link@sap.com>
2021-05-07 12:44:59 +0200

    switched to non-LTS 2.291

--- update/updateJenkins.sh
+++ update/updateJenkins.sh
@@ -13,21 +13,23 @@ packagerConfig="${PROJECT_ROOT}/jenkinsfile-runner-base-image/packager-config.ym
 function main() {
     echo "This is not automated yet - update packager-config.yml manually!"
 
-    printLatestLTSVersion
+    printLatestVersion true
     printCurrentBase
 }
 
-function printLatestLTSVersion() {
+function printLatestVersion() {
+    #local url='https://www.jenkins.io/changelog-stable/' #LTS
+    local url='https://www.jenkins.io/changelog/'
     local s
-    s=$(curl -sL 'https://www.jenkins.io/changelog-stable/') || {
-        echo >&2 "failed to download Jenkins LTS changelog"
+    s=$(curl -sL "$url") || {
+        echo >&2 "failed to download Jenkins changelog from $url"
         exit 1
     }
     s=$(<<<"$s" grep "^What's new in" -m1) || {
-        echo >&2 "failed to extract version from Jenkins LTS changelog"
+        echo >&2 "failed to extract version from Jenkins changelog ($url)"
         exit 1
     }
-    <<<"$s" sed -e 's/.*in /Latest LTS: /g'
+    <<<"$s" sed -e 's/.*in /Latest: /g'
 }
 
 function printCurrentBase() {
