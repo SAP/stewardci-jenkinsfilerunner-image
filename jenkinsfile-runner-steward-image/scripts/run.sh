ee645c097023ab59545b313d528631f015a8e2bc
Sergey Yurevich <sergey.yurevich@sap.com>
2023-06-21 17:14:25 +0000

    Add --write-log-retry-duration option

--- jenkinsfile-runner-steward-image/scripts/run.sh
+++ jenkinsfile-runner-steward-image/scripts/run.sh
@@ -115,6 +115,7 @@ function main() {
       -p /usr/share/jenkins/ref/plugins
       --runHome "${_JENKINS_HOME}"
       --no-sandbox
+      --write-log-retry-duration
       "${JFR_LOG_ARGS[@]}"
       ${JOB_NAME:+ --job-name "${JOB_NAME}"}
       ${RUN_NUMBER:+ --build-number "${RUN_NUMBER}"}
