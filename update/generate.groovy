ba0e050b6d31a24535dc85dfc08875ca7ced3f77
Roman Isbrecht <roman.isbrecht@sap.com>
2023-07-14 15:44:41 +0200

    Update to Jenkins core 2.387.3 and all plugins

--- update/generate.groovy
+++ update/generate.groovy
@@ -2,6 +2,7 @@ import groovy.json.JsonSlurper
 import groovy.json.JsonOutput
 
 // updateCenterUrl needs to be in sync with changelogUrl in updateJenkins.sh!
+updateCenterUrl = "https://updates.jenkins.io/update-center.json?version=2.387.3".toURL() // Version-specific
 updateCenterUrl = "https://updates.jenkins.io/stable/update-center.json".toURL()    //LTS
 //updateCenterUrl = "https://updates.jenkins.io/current/update-center.json".toURL() //LATEST
 
