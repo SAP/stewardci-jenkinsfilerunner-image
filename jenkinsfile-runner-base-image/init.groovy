5085985d9a28dc64e78929c5aa16792a42ffcac1
Andreas Brehmer <38314220+anbrsap@users.noreply.github.com>
2023-10-06 07:47:17 +0200

    Update Elasticsearch Logs plugin to v0.13.1 (#115)
    **Attention**: Environment variables for Elasticsearch Logs plugin
    configuration have changed incompatibly!

--- jenkinsfile-runner-base-image/init.groovy
+++ jenkinsfile-runner-base-image/init.groovy
@@ -1,2 +1,9 @@
+import java.util.logging.Level
+import java.util.logging.Logger
+
 // Allow undefined parameters in pipeline
 System.setProperty("hudson.model.ParametersAction.keepUndefinedParameters", "true")
+
+// Silence org.komamitsu.fluency.Fluency because it logs an error if the buffer is full,
+// which happens very often in our scenario.
+Logger.getLogger("org.komamitsu.fluency.Fluency").setLevel(Level.OFF)
